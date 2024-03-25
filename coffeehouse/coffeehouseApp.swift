0b04a5822954f6678774986a3d12a417fe91a794
joshbourget <joshtbourget@gmail.com>
2024-03-25 03:40:07 -0500

    first commit

---
+++ coffeehouse/coffeehouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeehouseApp.swift
+//  coffeehouse
+//
+//  Created by Josh Bourget on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct coffeehouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
